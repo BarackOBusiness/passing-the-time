e56ab67e408555e30fe50eff8c45dc5d9002ee27
BarackOBusiness <66978282+BarackOBusiness@users.noreply.github.com>
2023-05-29 18:52:35 +0000

    the hello world of rendering + vec3 utility crate

---
+++ src/vec.rs
@@ -0,0 +1,167 @@
+use std::ops::{Index, IndexMut, Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Div, DivAssign};
+use std::{fmt,fmt::Display};
+
+#[derive(Clone, Copy)]
+pub struct Vec3 {
+    e: [f64; 3]
+}
+
+// Abstractions to vec3, for organizational purposes
+pub type Point3 = Vec3;
+pub type Color = Vec3;
+
+impl Vec3 {
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
+        Vec3 {
+            e: [e0, e1, e2]
+        }
+    }
+
+    pub fn x(self) -> f64 {
+        self[0]
+    }
+    
+    pub fn y(self) -> f64 {
+        self[1]
+    }
+    
+    pub fn z(self) -> f64 {
+        self[2]
+    }
+    
+    pub fn dot(self, other: Vec3) -> f64 {
+        self[0] * other[0] + self[1] * other[1] + self[2] * other[2]
+    }
+    
+    pub fn length(self) -> f64 {
+        self.dot(self).sqrt()
+    }
+    
+    pub fn cross(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self[1] * other[2] - self[2] * other[1],
+                self[2] * other[0] - self[0] * other[2],
+                self[0] * other[1] - self[1] * other[0]
+            ]
+        }
+    }
+    
+    pub fn normalized(self) -> Vec3 {
+        self / self.length()
+    }
+    
+    pub fn format_color(self) -> String {
+        format!("{} {} {}", (255.999 * self[0]) as u64,
+                            (255.999 * self[1]) as u64,
+                            (255.999 * self[2]) as u64)
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f64;
+
+    // Return a reference to the f64 indexed
+    fn index(&self, index: usize) -> &f64 {
+        &self.e[index]
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    // The same thing but it's a mutable reference instead
+    fn index_mut(&mut self, index: usize) -> &mut f64 {
+        &mut self.e[index]
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [self[0] + other[0], self[1] + other[1], self[2] + other[2]]
+        }
+    }
+}
+
+impl AddAssign for Vec3 {
+    // Assigns the resultant value to the self being operated on
+    fn add_assign(&mut self, other: Vec3) -> () {
+        *self = Vec3 {
+            e: [self[0] + other[0], self[1] + other[1], self[2] + other[2]]
+        }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [self[0] - other[0], self[1] - other[1], self[2] - other[2]]
+        }
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Vec3) -> () {
+        *self = Vec3 {
+            e: [self[0] - other[0], self[1] - other[1], self[2] - other[2]]
+        };
+    }
+}
+
+// Multiply a Vec3 by a float
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, other: f64) -> Vec3 {
+        Vec3 {
+            e: [self[0] * other, self[1] * other, self[2] * other]
+        }
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, other: f64) -> () {
+        *self = Vec3 {
+            e: [self[0] * other, self[1] * other, self[2] * other]
+        };
+    }
+}
+
+// Multiply a float by a Vec3
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [self * other[0], self * other[1], self * other[2]]
+        }
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, other: f64) -> Vec3 {
+        Vec3 {
+            e: [self[0] / other, self[1] / other, self[2] / other]
+        }
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, other: f64) -> () {
+        *self = Vec3 {
+            e: [self[0] / other, self[1] / other, self[2] / other]
+        };
+    }
+}
+
+impl Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {}, {})", self[0], self[1], self[2])
+    }
+}
+
