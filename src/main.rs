0c6eb15225ba227e6a857fb677bbbc6ea2f527d7
BarackOBusiness <66978282+BarackOBusiness@users.noreply.github.com>
2023-05-30 17:24:48 +0000

    Positionable camera, switched to lambertian diffuse

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ use std::{io::{stderr, Write}, rc::Rc};
 
 use rand::prelude::*;
 
-use vec::{Point3, Color};
+use vec::{Vec3, Point3, Color};
 use ray::Ray;
 use hit::{Hit, World};
 use sphere::Sphere;
 use camera::Camera;
-use material::{Hemispheral, Metal};
+use material::{Lambertian, Hemispheral, Metal, Dielectric};
 
 fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
     if depth <= 0 {
@@ -45,23 +45,37 @@ fn main() {
 
     // World
     let mut world = World::new();
-    let mat_ground = Rc::new(Hemispheral::new(Color::new(0.8, 0.8, 0.0)));
-    let mat_center = Rc::new(Hemispheral::new(Color::new(0.7, 0.3, 0.3)));
+
+    let mat_ground = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.8)));
+    let mat_center = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.4)));
     let mat_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
     let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+
+    let mat_back = Rc::new(Dielectric::new(1.5));
+    let mat_back_inner = Rc::new(Dielectric::new(1.5));
 
     let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground);
     let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center);
     let sphere_left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left);
     let sphere_right = Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right);
 
+    let sphere_back = Sphere::new(Point3::new(0.0, -100.0 + f64::sin(std::f64::consts::FRAC_PI_3), -101.0 + f64::cos(std::f64::consts::FRAC_PI_3)), 0.5, mat_back);
+    let sphere_back_inner = Sphere::new(Point3::new(0.0, -100.0 + f64::sin(std::f64::consts::FRAC_PI_3), -101.0 + f64::cos(std::f64::consts::FRAC_PI_3)), 0.5, mat_back_inner);
+
     world.push(Box::new(sphere_ground));
     world.push(Box::new(sphere_center));
     world.push(Box::new(sphere_left));
     world.push(Box::new(sphere_right));
 
+    world.push(Box::new(sphere_back));
+    world.push(Box::new(sphere_back_inner));
+
     // Camera
-    let cam = Camera::new();
+    let cam = Camera::new(Point3::new(-2.0, 2.0, 1.0),
+                                    Point3::new(0.0, 0.0, -1.0), 
+                                    Vec3::new(0.0, 1.0, 0.0), 
+                                    90.0, 
+                                    ASPECT_RATIO);
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
--- src/material.rs
+++ src/material.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use super::ray::Ray;
 use super::hit::HitRecord;
 use super::vec::{Vec3, Color};
@@ -29,6 +31,7 @@ impl Scatter for Lambertian {
     }
 }
 
+// I'm not sure if this implementation of hemispheral diffuse is correct in the new material module
 pub struct Hemispheral {
     albedo: Color
 }
@@ -76,5 +79,49 @@ impl Scatter for Metal {
         } else {
             None
         }
+    }
+}
+
+pub struct Dielectric {
+    ior: f64
+}
+
+impl Dielectric {
+    pub fn new(index_of_refraction: f64) -> Dielectric {
+        Dielectric { ior: index_of_refraction }
+    }
+
+    fn reflectanace(cosine: f64, ref_idx: f64) -> f64 {
+        // Use schlick's approximation for reflectance
+        let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx).powi(2));
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
+}
+
+impl Scatter for Dielectric {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
+        let refraction_ratio = if rec.front_face {
+            1.0 / self.ior
+        } else {
+            self.ior
+        };
+
+        let unit_direction = r_in.direction().normalized();
+        let cos_theta = ((-1.0) * unit_direction).dot(rec.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
+
+        let mut rng = rand::thread_rng();
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let will_reflect = rng.gen::<f64>() < Self::reflectanace(cos_theta, refraction_ratio);
+
+        let direction = if cannot_refract || will_reflect {
+            unit_direction.reflect(rec.normal)
+        } else {
+            unit_direction.refract(rec.normal, refraction_ratio)
+        };
+
+        let scattered = Ray::new(rec.p, direction);
+
+        Some((Color::new(1.0, 1.0, 1.0), scattered))
     }
 }
